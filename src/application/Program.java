4f4601d52703d9b5450da2c05f20de1ec71c96cc
Jean Castro <jeanlucascastro@gmail.com>
2020-09-07 22:50:16 -0300

    Quase terminado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,59 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat simpleDate = new SimpleDateFormat("dd/MM/yyyy");
 		
-
+		System.out.println("Digite o nome do comprador ");
+		String nome = sc.nextLine();
+		System.out.println("Digite o email do comprador ");
+		String email = sc.nextLine();
+		System.out.println("Digite o ano de nascimento ");
+		Date nasc = simpleDate.parse(sc.next());
+		
+		Client client = new Client(nome, email, nasc);
+		
+		System.out.println("Entre os dados do seu pedido ");
+		OrderStatus status = OrderStatus.valueOf(sc.next());
+		
+		Order order = new Order (new Date(), status, client);
+				
+		
+		System.out.println("quantos items voce vai pedir ");
+		int n = sc.nextInt();
+		for (int i =0; n < i; i++) {
+			System.out.println("nome do produto");
+			String nomeProduto = sc.nextLine();
+			System.out.println("Qual o preço do produto ? ");
+			double priceProduto = sc.nextDouble();
+			
+			Product product = new Product (nomeProduto,priceProduto);
+			
+			int quantidadeProduto = sc.nextInt();
+			
+			OrderItem orderItem = new OrderItem (quantidadeProduto, priceProduto, product);
+			
+			order.addItem(orderItem);
+			
+		}
+		
+		
+		
 	}
 
 }
--- src/entities/OrderItem.java
+++ src/entities/OrderItem.java
@@ -1,5 +1,39 @@
 package entities;
 
 public class OrderItem {
+	Integer quantidade;
+	Double preco;
+	
+	private Product product;
+	
+	public OrderItem(Integer quantidade, Double preco, Product product) {
+		this.quantidade = quantidade;
+		this.preco = preco;
+		this.product = product;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public Product getProduct() {
+		return product;
+	}
 
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	public double subTotal() {
+		return quantidade * preco;
+	}
+
+	
 }
